87477c158415259f64a2bfa45fb3e5356f6ab915
Javier Perez <jpere0500@gmail.com>
2023-05-27 21:57:25 +0100

    acabao?

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const insertarNombres = document.getElementById("input-nombres");
 const clearBtn = document.getElementById("boton-limpiar");
 let equipo1 = document.getElementById("players");
 let equipo2 = document.getElementById("players2");
-let unsortedNames = ["javier", "xploid", "jon", "joel", "slope", "joan", "francisco", "pakflow", "pedro", "juan"];
-let sorting;
+let unsortedNames = [];
 let sortedNames = [];
+let listTeam1 = "";
+let listTeam2 = "";
 
 
 insertBtn.addEventListener("click", function() {
@@ -18,17 +19,21 @@ insertBtn.addEventListener("click", function() {
 });
 
 randomBtn.addEventListener("click", function() {
+    let contador = 0;
+    contador = 0;
+    listTeam1 = "";
+    listTeam2 = "";
     while (unsortedNames.length > 0 ){
+        let sorting;
         sorting = Math.floor(Math.random() * unsortedNames.length);
-        unsortedNames.splice(sorting, 1);
         sortedNames.push(unsortedNames[sorting]);
-        console.log(unsortedNames);
-        console.log(sorting);
-        console.log(unsortedNames[sorting]);
-        //console.log(sortedNames);
-       // console.log(sortedNames.length)
+        unsortedNames.splice(sorting, 1);
+        contador += 1;
+        console.log(contador);
     }
-    if (sortedNames === 10){
+    if (contador === 10){
+        equipo2.innerHTML = "";
+        equipo1.innerHTML = "";
         for (let i = 0; i < sortedNames.length; i++) {
             listTeam1 += "<li>" + sortedNames[i] + "</li>";
             i++
@@ -45,19 +50,21 @@ clearBtn.addEventListener("click", function() {
     equipo2.innerHTML = "";
     listTeam1 = "";
     listTeam2 = "";
-    sorting;
     sortedNames = [];
+    listTeam1 = "";
+    listTeam2 = "";
 });
 
 function renderNames() {
-    let listTeam1 = "";
-    let listTeam2 = "";
+    listTeam1 = "";
+    listTeam2 = "";
     console.log(unsortedNames);
     for (let i = 0; i < unsortedNames.length; i++) {
-        listTeam1 += "<li>" + unsortedNames[i] + "</li>";
-        i++
+        listTeam1 += "<li>" + unsortedNames[i] + "</li>";  
+        i++;
         listTeam2 += "<li>" + unsortedNames[i] + "</li>";
     }  
     equipo2.innerHTML = listTeam2;
     equipo1.innerHTML = listTeam1;
 }
+
